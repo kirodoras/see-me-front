855722ae455457a992a2183df09edf1b4a6879e1
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-28 18:32:12 -0300

    feat: update home component

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Styled from 'styled-components';
+
+export default function Home() {
+    return (
+        <HomeStyled>
+            <Header />
+            <Main />
+        </HomeStyled>
+    );
+}
+
+const HomeStyled = Styled.div`
+    width: 100%;
+    max-width: 100%;
+    color: white;
+`;
