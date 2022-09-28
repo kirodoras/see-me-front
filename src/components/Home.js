4c8ba3f577cd7d532b271c54917c55a8c3871537
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-27 23:25:47 -0300

    feat: init home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import Styled from 'styled-components';
+
+export default function Home() {
+    return (
+        <HomeStyled>
+            Home
+        </HomeStyled>
+    );
+}
+
+const HomeStyled = Styled.div`
+   color: white;
+`;
