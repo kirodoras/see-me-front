1ab24928d65003606b306bb2eec25bdbe3cdf87f
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-28 18:32:37 -0300

    feat: add user context provider to app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
 
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 import ResetCss from "./styles/ResetCss";
 import GlobalCss from "./styles/GlobalCss";
 
 export default function App() {
+    const [user, setUser] = useState("");
+
     return (
-        <BrowserRouter>
-            <ResetCss />
-            <GlobalCss />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="*" element={<>Not found</>} />
-            </Routes>
-        </BrowserRouter>
+        <UserContext.Provider value={{ user, setUser }}>
+            <BrowserRouter>
+                <ResetCss />
+                <GlobalCss />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<>Not found</>} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
