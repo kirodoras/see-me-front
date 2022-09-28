dc31a66a02d3a4a6179865672748a00471b8b7d6
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-27 23:27:00 -0300

    style: set global css

---
+++ src/styles/GlobalCss.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalCss = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    input, button {
+        border: none;
+    }
+    a {
+        text-decoration: none;
+    }
+    body {
+        display: flex;
+        justify-content: center;
+        width: 100vw;
+        max-width: 100%;
+        height: 100vh;
+        background: #0f152b;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        font-family: 'Poppins', sans-serif;
+    }
+    .root {
+        width: 90%;
+        max-width: 90%;
+        height: 90%;
+        margin-top: 1.3rem;
+        border: 1px solid white;
+    }
+`;
+
+export default GlobalCss;
