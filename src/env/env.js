68aa6bfb97136a13db4708052dbf7a5b506e4397
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-04 14:31:59 -0300

    feat: create env variable to backend url

--- src/env/env.js
+++ src/env/env.js
@@ -1,4 +1,5 @@
 const googleApiUrl = process.env.REACT_APP_GOOGLE_API_URL;
 const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const backEndUrl = process.env.REACT_APP_BACKEND_URL;
 
-export { googleApiUrl, googleClientId };
+export { googleApiUrl, googleClientId, backEndUrl };
