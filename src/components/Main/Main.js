087438e9ea0be82596739c208593f044e3ae56cb
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-05 11:32:43 -0300

    feat: update main to call meeting form

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,9 @@
 import Styled from 'styled-components';
-
+import MeetingForm from '../MeetingForm/MeetingForm';
 export default function Main() {
     return (
         <MainStyled>
-            <div>
-            </div>
+            <MeetingForm />
             <span>
             </span>
         </MainStyled>
@@ -18,19 +17,8 @@ const MainStyled = Styled.main`
     color: #f7f0f5;
     padding-top: 3rem;
     display: flex;
-    gap: 2rem;
+    gap: 1rem;
 
-    &>div {
-        width: 60%;
-        max-width: 100%;
-        height: 100%;
-        background: rgba( 183, 184, 191, 0.45 );
-        box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-        backdrop-filter: blur( 10px );
-        -webkit-backdrop-filter: blur( 10px );
-        border-radius: 10px;
-        border: 1px solid rgba( 255, 255, 255, 0.18 );
-    }
     &>span {
         width: 40%;
         max-width: 100%;
@@ -46,6 +34,7 @@ const MainStyled = Styled.main`
     @media(max-width: 68.75rem) {
         flex-direction: column;
         padding-top: 2rem;
+        gap: 0.5rem;
         &>div {
             width: 100%;
         }
