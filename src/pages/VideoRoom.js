4a996057e46f62bed53eaef703d0699d5df73250
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-06 15:53:24 -0300

    feat: create video room page

--- src/pages/VideoRoom.js
+++ src/pages/VideoRoom.js
@@ -1,7 +1,7 @@
 import Styled from 'styled-components';
 import { useState } from "react";
 import VideoCall from '../components/Video/VideoCall';
-
+import { useNavigate } from 'react-router-dom';
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 import { useContext } from "react";
 import ChannelContext from "../contexts/ChannelContext";
@@ -10,12 +10,19 @@ import { appId } from "../env/env";
 export default function VideoRoom() {
     const [inCall, setInCall] = useState(true);
     const { channel } = useContext(ChannelContext);
-    if (!channel.channelName || !channel.token) return null;
+    const navigate = useNavigate();
+    if (!channel.channelName && !channel.token) {
+        navigate("/");
+    }
     const { channelName, token } = channel;
     const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
     const useClient = createClient(config);
     const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
-
+    const { error } = useMicrophoneAndCameraTracks;
+    if (error) {
+        navigate("/");
+        alert("Devices not found");
+    }
     return (
         <VideoRoomStyled>
             {inCall ? <VideoCall
